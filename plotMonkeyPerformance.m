00907272439ca352fed14d505830dfba8e2ead37
lubavas <luvasilieva@mail.ru>
2022-04-13 17:31:33 +0200

    plots performance over all trials for one file

---
+++ plotMonkeyPerformance.m
@@ -0,0 +1,24 @@
+function plotMonkeyPerformance(datafile)
+% This code smoothes the monkey performance over all the trial outcomes 
+% (rewarded, initiated and skipped)data with 'lowess' method and % plots 
+% it.
+%
+% Example use:
+% plotMonkeyPerformance('Y:\Data\Linus\20220413\Lin2022-04-13_03.mat')
+%
+
+load(datafile, 'trial')
+
+rewarded_trial_ids = [trial.rewarded] == 1;
+
+y = smooth(rewarded_trial_ids, 30, 'lowess');
+
+figure, plot(y)
+ylim([0 1])
+
+lastSlash = find(datafile == '\', 1, 'last');
+ttl = datafile(lastSlash+1:end-4);
+
+title(ttl, 'interpreter', 'none')
+xlabel('Trial Number')
+ylabel('Performance')
